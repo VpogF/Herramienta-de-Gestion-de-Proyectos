e7b1eea9602254ce3750e2ad46f7dea57fdc2d9d
VpogF <vpoglonig2324@politecnics.barcelona>
2024-11-29 20:05:41 +0100

    registro y login
    desarrollo del registro y login

--- registro.php
+++ registro.php
@@ -1,3 +1,10 @@
+<?php
+    require_once('./php_librarys/bd.php');
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,27 +35,30 @@
 
     <div class="container bg-beige pt-3 pb-3 mt-5 d-flex justify-content-center cont-1 col col-sm-3 col col-sx-1">
 
-        <form class="login-regitro-form" action="">
+        <form class="login-regitro-form" action="./php_controllers/userController.php" method="POST">
             <div class="cont-titulo-1">
-                <h3 class="card-title text-pink">Iniciar Sesión</h3>
+                <h3 class="card-title text-pink">Crear Cuenta</h3>
             </div>
             <div class="input-cont">
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label text-danger label-class">Nombre Usuario</label>
-                    <input type="email" class="form-control input-class" id="exampleFormControlInput1">
+                    <input type="text" class="form-control input-class" name="nom_user" id="exampleFormControlInput1">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label text-danger label-class">Email</label>
+                    <input type="email" class="form-control input-class" name="email_user" id="exampleFormControlInput1 ">
                 </div>
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label text-danger label-class">Constraseña</label>
-                    <input type="email" class="form-control input-class" id="exampleFormControlInput1">
+                    <input type="password" class="form-control input-class" name="pass_user" id="exampleFormControlInput1">
                 </div>
             </div>
-            <button type="button" class="btn btn-pink text-danger regist-btn">Ingresar</button>
-            <h6 class="text-primary text-registro">¿No tienes cuenta?<a class="text-danger" href="./registro.html">Regístrate</a></h6>
+            <button type="submit" class="btn btn-pink text-danger regist-btn" name="insert">Registrarse</button>
         </form>
 
     </div>
 
-
+    
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 
